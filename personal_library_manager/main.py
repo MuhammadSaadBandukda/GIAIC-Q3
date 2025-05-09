235291da6332dd1ca91a7140eea6bc9c0708ba6a
Muhammad Saad <saadsaleem997@gmail.com>
2025-05-09 22:20:47 +0500

    library management completed

--- personal_library_manager/main.py
+++ personal_library_manager/main.py
@@ -1,8 +1,152 @@
+import time
+from books import *
+
 def main():
     while True:
+        books = None
+        with open("library.txt",'r') as file:
+            # file = open("library.txt",'r+')
+            books = read_data(file)
+        op = display_menu()
         match op:
-            case 'e':
-                break
+            case '1':
+                add_book(books)
+            case '2':
+                remove_book(books)
+            case '3':
+                search_book(books)
+            case '4':
+                display_books(books)
+                time.sleep(3)
+            case '5':
+                display_statistics(books)
+                time.sleep(3)
+            case '6':
+                print("Exiting...",end='\r')
+                time.sleep(1)
+                raise SystemExit
+            case _:
+                print("Select valid!")
+        # file.seek(0)
+        with open('library.txt','w') as file:
+            sync_data(file,books)
+
+
+# def display_menu():
+#     menu="""
+#         Welcome to your Personal Library Manager!  
+#         1. Add a book  
+#         2. Remove a book  
+#         3. Search for a book  
+#         4. Display all books  
+#         5. Display statistics  
+#         6. Exit  
+#         """ 
+#     print(menu)         
+#     return input("Enter your choice: ")
+
+# def add_book(books:list[dict]):
+#     title=input("Enter the book tittle: ")
+#     author=input("Enter the book author: ")
+#     year=input("Enter the book year: ")
+#     genre=input("Enter the book genre: ")
+#     while True:
+#         innput = input("Have youu read that book Y/N: ")
+#         if innput == 'Y' or innput == 'y':
+#             isRead=True
+#             break
+#         elif innput == 'N' or innput == 'n':
+#             isRead=False
+#             break
+#         else:
+#             print("PLease Enter 'Y' or 'N'")
+
+#     new_book = {
+#         "title":title,
+#             "author":author,
+#             "publication_year":year,
+#             "genre":genre,
+#             "isRead":isRead
+#     }
+#     books.append(new_book)
+#     # print(books)
+
+# def remove_book(books:list[dict]):
+#     title=input("Enter the book title to remove: ")
+#     count = 0
+#     for book in books:
+#         if title == book["title"]:
+#             count+=1
+#             books.remove(book)
+#     if (count>0):
+#         print(f"{count} book removed!...")
+#     else:
+#         print("No book found with such a title")
+    
+
+# def sync_data(file, books):
+#     for book in books:
+#         line = f"{book['title']}|{book['author']}|{book['publication_year']}|{book['genre']}|{book['isRead']}\n"
+#         file.write(line)
+
+
+# def read_data(file) -> list[dict]:
+#     books = []
+#     for line in file:
+#         parts = line.strip().split('|')
+#         if len(parts) == 5:
+#             title, author, year, genre, isRead = parts
+#             books.append({
+#                 "title": title,
+#                 "author": author,
+#                 "publication_year": year,
+#                 "genre": genre,
+#                 "isRead": isRead == "True"
+#             })
+#     return books
+
+    
+
+# def display_books(books: list[dict]):
+#     for i, book in enumerate(books, start=1):
+#         read_status = "Read" if book['isRead'] else "Not Read"
+#         print(f"{i}. {book['title']} by {book['author']} ({book['publication_year']}) - {book['genre']} [{read_status}]\n")
+
+# def display_statistics(books: list[dict]):
+#     print(f"Total books: {len(books)}")
+#     count = 0
+#     for book in books:    
+#         if book['isRead']:
+#             count+=1
+#     per = count/len(books)*100
+#     print(f"Read Percentage: {per}")
+
+
+# def search_book(books:list[dict]):
+#     message = """Search By: 
+# 1.Title
+# 2.Author
+# Enter Your Choice:
+# """
+#     search_by = int(input(message))
+#     if search_by == 1:
+#         title = input("Enter title: ")
+#         count = 0
+#         for book in books:
+#             if title in book['title']:
+#                 count+=1
+#                 print(f"{count}. {book['title']} by {book['author']} ({book['publication_year']}) - {book['genre']} {book['isRead']}")
+            
+#     elif search_by == 2:
+#         author = input("Enter author: ")
+#         count = 0
+#         for book in books:
+#             if author in book['author']:
+#                 count+=1
+#                 print(f"{count}. {book['title']} by {book['author']} ({book['publication_year']}) - {book['genre']} {book['isRead']}")
+#     else:
+#         print("Invalid Choice...")
+    
 
 if __name__ == "__main__":
     main()
